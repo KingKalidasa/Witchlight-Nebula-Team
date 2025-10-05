336c31e520b158e2400d915965ba15a096630856
Luis Quinones <thelfq@gmail.com>
2025-10-04 19:58:23 -0500

    Added dotenv and serpapi with npm.
    filled out get_weather to return json information.

--- mcp-server/tools/get_weather.ts
+++ mcp-server/tools/get_weather.ts
@@ -2,8 +2,12 @@
 // Purpose: Defines and registers a tool to query weather data for the MCP server.
 // ------------------------------------------------------------------
 
-import { date, z } from "zod"; // zod is used to define and validate input/output schemas
+import { z } from "zod"; // zod is used to define and validate input/output schemas
 import type { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { getJson } from "serpapi";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 
 export function register(server: McpServer) {
@@ -20,12 +24,36 @@ export function register(server: McpServer) {
       }).shape,
       outputSchema: z.object({ result: z.number() }).shape, // output will contain a "result" number
     },
-
+    
     async ({ location_name, date_YYYYMMDD }) => {
       
       // const output = { result: location_name + date_YYYYMMDD };
-      const output = { result: 80 };
+      const query = "what is the weather in " + location_name + " on " + date_YYYYMMDD.toString;
+      const json= await new Promise((resolve, reject) => {
+        getJson(
+        {
+          api_key: process.env.SERPAPI_KEY,
+          engine: "google",
+          q: query,
+          google_domain: "google.com",
+          gl: "us",
+          hl: "en"
+        },
+        (result) => {
+          if (result) resolve(result);
+          else reject(new Error("No result from SerpAPI"));
+        }
+      );
+  });
 
+ const body = (json as any)?.answer_box;
+ let output;
+
+  if (body && body.temp) {
+    output = { result: body.temp };
+  } else {
+    output = { result: "not found" };
+  }
       // Return both a text representation and a structured JSON object
       return {
         content: [
